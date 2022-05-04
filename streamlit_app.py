3aaf3db040aafe400f18a6d1cd672d3c18f8f2cc
zsnowflake122 <104879758+zsnowflake122@users.noreply.github.com>
2022-05-03 20:35:55 -0700

    Update streamlit_app.py
    Add defaults

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,5 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 streamlit.dataframe(my_fruit_list)
